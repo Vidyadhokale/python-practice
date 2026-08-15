5dee7a375d94767c6131d9944c9e03d7027780ae
Vidyadhokale <vidyadhokale4@gmail.com>
2026-08-15 23:02:24 +0530

    Add files via upload

---
+++ LargestNo_fun.py
@@ -0,0 +1,6 @@
+def largest(a,b):
+    if a>b :
+        print(f"{a} is Largest")
+    elif b>a:
+        print(f"{b} is  Largest")
+largest(20,15)
---
+++ evenOdd_fun.py
@@ -0,0 +1,6 @@
+def even_odd(a):
+    if a % 2==0:
+        print(f"{a} is Even")
+    else:
+        print("Odd")
+even_odd(4)
