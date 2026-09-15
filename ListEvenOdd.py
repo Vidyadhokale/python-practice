1ed6e468a4e63043f500f434cc9cf88e3a5f7ec1
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-16 00:05:12 +0530

    Fix even and odd number classification
    Refactor even and odd number categorization logic.

--- ListEvenOdd.py
+++ ListEvenOdd.py
@@ -4,10 +4,10 @@ odd=[]
 for num in range(10):
     num=int(input("Enter 10 Numbers:"))
     list1.append(num)
+    if num % 2==0:
+        even.append(num)
+    else:
+        odd.append(num)
 print("List : ",list1)
-if num % 2==0:
-    even.append(num)
-else:
-    odd.append(num)
-print("Evenv : ",even)
+print("Even : ",even)
 print("Odd : ",odd) 
