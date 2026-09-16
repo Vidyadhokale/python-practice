32cfc1735c3cdf17b3159b4e2078d239b1705f1b
Vidyadhokale <vidyadhokale4@gmail.com>
2026-09-16 18:30:12 +0530

    Add files via upload

---
+++ AreaOfTraingle.py
@@ -0,0 +1,4 @@
+base=float(input("Enter The Base:"))
+height=float(input("Enter The Height:"))
+area=0.5*base*height
+print("Area Of  Traingle :",area)
---
+++ SwapNum.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter First Number:"))
+n2=int(input("Enter Second Number:"))
+print("First Number:",n1)
+print("Second Number:",n2)
+a=n1
+n1=n2
+n2=a
+print("First Number after swap:",n1)
+print("Second Number after swap:",n2)
